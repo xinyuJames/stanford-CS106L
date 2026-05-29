28424a2fd2805519a1012474d984cee55b2ef7ca
Xinyu Liu <liuxinyujames@gmail.com>
2026-05-29 14:10:11 -0400

    [finish] a4

--- assignment4/spellcheck.cpp
+++ assignment4/spellcheck.cpp
@@ -12,15 +12,16 @@ std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
 Corpus tokenize(std::string& source) {
   /* TODO: Implement this method */
-  // find all iteratr for space
-  std::vector<std::string::iterator> space_it;
-  space_it = find_all(source.begin(), source.end(), std::isspace);
 
-  // token set, inserter to insert into
+  // find all iterator for space
+  auto space_it = find_all(source.begin(), source.end(), [](char c){ return std::isspace(c); });
+
+  // token set, insert Token to end of tokens iterator
   Corpus tokens;
-  std::transform(space_it.begin(), space_it.end()-1, space_it.begin()-1,
-    std::inserter(tokens, tokens.end()), [source](auto begin, auto end){ return (new Token(source, begin, end)); });
+  std::transform(space_it.begin(), space_it.end()-1, space_it.begin()+1,
+    std::inserter(tokens, tokens.end()), [&source](auto beg, auto en){ return (Token(source, beg, en)); });
 
+  // erase token with empty content
   std::erase_if(tokens, [](Token t){ return t.content.empty(); });
   
   return tokens;
@@ -28,7 +29,24 @@ Corpus tokenize(std::string& source) {
 
 std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
   /* TODO: Implement this method */
-  return std::set<Misspelling>();
+  auto view = source 
+    // filter out correct words
+    | std::ranges::views::filter([&dictionary](const Token& cur){ return !dictionary.contains(cur.content); })
+    // transform into Misspelling type, with suggestions 1 char away
+    | std::ranges::views::transform([&dictionary](const Token& cur){
+      // make one_away set
+      auto one_away = dictionary | std::ranges::views::filter([&cur](const std::string& dic){
+        return levenshtein(cur.content, dic) == 1;
+      });
+      std::set<std::string> suggestions(one_away.begin(), one_away.end());
+
+      return Misspelling(cur, suggestions);
+    })
+    // filter out misspelling with no suggestion
+    | std::ranges::views::filter([](const Misspelling& cur){ return !cur.suggestions.empty(); });
+
+  // return as a set
+  return std::set<Misspelling>(view.begin(), view.end());
 };
 
 /* Helper methods */
