7aeda0b9db10e9ad18ba914fc73da4e15f79f5bf
Xinyu Liu <liuxinyujames@gmail.com>
2026-05-29 17:57:17 +0800

    [update] a4

--- assignment4/spellcheck.cpp
+++ assignment4/spellcheck.cpp
@@ -12,7 +12,18 @@ std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
 Corpus tokenize(std::string& source) {
   /* TODO: Implement this method */
-  return Corpus();
+  // find all iteratr for space
+  std::vector<std::string::iterator> space_it;
+  space_it = find_all(source.begin(), source.end(), std::isspace);
+
+  // token set, inserter to insert into
+  Corpus tokens;
+  std::transform(space_it.begin(), space_it.end()-1, space_it.begin()-1,
+    std::inserter(tokens, tokens.end()), [source](auto begin, auto end){ return (new Token(source, begin, end)); });
+
+  std::erase_if(tokens, [](Token t){ return t.content.empty(); });
+  
+  return tokens;
 }
 
 std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
